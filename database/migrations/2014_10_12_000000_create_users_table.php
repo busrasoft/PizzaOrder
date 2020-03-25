c521a021c5a96d914c20a8d49866b7c9e59990c6
unknown <busrasof@gmail.com>
2020-03-25 22:15:55 +0300

    bootstrap.min  is added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('address')->nullable();
             $table->integer('phone')->nullable();
+            // $table->integer('admin_auth')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,57 +1,83 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Laravel 7 CRUD Application</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href = {{ asset("css/bootstrap.min.css") }} rel="stylesheet" />
+
 </head>
 <body>
-  
-<div class="container">
-    @yield('content')
-    
-</div>
-   
-    <script src={{ asset("public/frontend/js/jquery.js") }} ></script>
-    <script src={{ asset("public/frontend/js/popper.js") }} ></script>
-    <script src={{ asset("public/frontend/js/bootstrap.min.js") }} ></script>
-    <script src={{ asset("public/frontend/js/jquery-1.11.1.min.js") }} ></script>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<!------ Include the above in your HEAD tag ---------->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-    <style>
-        .dropdown-submenu {
-            position: relative;
-        }
+                    </ul>
 
-        .dropdown-submenu> a:after {
-            content: ">";
-            float: right;
-        }
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-        .dropdown-submenu>.dropdown-menu {
-            top: 0;
-            left: 100%;
-            margin-top: 0px;
-            margin-left: 0px;
-        }
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-        .dropdown-submenu:hover>.dropdown-menu {
-            display: block;
-        }
-    </style>
-    
-    <script>
-        $(".btn-group, .dropdown").hover(
-            function () {
-                $('>.dropdown-menu', this).stop(true, true).fadeIn("fast");
-                $(this).addClass('open');
-            },
-            function () {
-                $('>.dropdown-menu', this).stop(true, true).fadeOut("fast");
-                $(this).removeClass('open');
-            });
-    </script>
-        
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="container">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
